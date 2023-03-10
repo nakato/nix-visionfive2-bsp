f58e46d2089bfc29888f64f2cd53ef6af0285a90
Sachi King <nakato@nakato.io>
2023-03-10 03:33:05 +0000

    Copy expandOnBoot from nixpkgs sd-card.nix
    Make room on the sd-card for doing things.  Thre's probably not enough
    room remaining to even pull down the tooling to do resizing, especially
    as it's probably not cached.

--- sd-image.nix
+++ sd-image.nix
@@ -116,5 +116,33 @@ in {
         dd conv=notrunc if=$root_fs of=$img seek=$rootPartStart
       '';
     }
+
+    # Copy from nixpkgs sd-card.nix
+    boot.postBootCommands = ''
+      # On the first boot do some maintenance tasks
+      if [ -f /nix-path-registration ]; then
+        set -euo pipefail
+        set -x
+        # Figure out device names for the boot device and root filesystem.
+        rootPart=$(${pkgs.util-linux}/bin/findmnt -n -o SOURCE /)
+        bootDevice=$(lsblk -npo PKNAME $rootPart)
+        partNum=$(lsblk -npo MAJ:MIN $rootPart | ${pkgs.gawk}/bin/awk -F: '{print $2}')
+
+        # Resize the root partition and the filesystem to fit the disk
+        echo ",+," | sfdisk -N$partNum --no-reread $bootDevice
+        ${pkgs.parted}/bin/partprobe
+        ${pkgs.e2fsprogs}/bin/resize2fs $rootPart
+
+        # Register the contents of the initial Nix store
+        ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration
+
+        # nixos-rebuild also requires a "system" profile and an /etc/NIXOS tag.
+        touch /etc/NIXOS
+        ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+
+        # Prevents this from running on later boots.
+        rm -f /nix-path-registration
+      fi
+    '';
   ) { uboot = vf2uboot; };
 }
